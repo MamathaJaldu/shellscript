36ddd7711fc2c9cd530a63ed81f7d013663d500e
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-12 14:56:43 -0500

    added arrays

---
+++ 05-datatypes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sum1=$1
+sum2=$2
+sum=$(($sum1+$sum2))
+echo "Total:$sum"
---
+++ 06-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "orange")
+echo "First value:$FRUITS[0]"
+echo "Second value:$FRUITS[1]"
+echo "Third value:$FRUITS[2]"
+
