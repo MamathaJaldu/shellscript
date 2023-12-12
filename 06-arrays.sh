42d8055d73e7a235ec2d3d6b52ed116821d09d67
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-12 15:14:11 -0500

    changes added

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 FRUITS=("apple" "banana" "orange")
-echo "First value:$FRUITS[0]"
-echo "Second value:$FRUITS[1]"
-echo "Third value:$FRUITS[2]"
+echo "First value:${FRUITS[0]}"
+echo "Second value:${FRUITS[1]}"
+echo "Third value:${FRUITS[2]}"
+echo "All values:${FRUITS[@]}"
 
