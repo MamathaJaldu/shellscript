1b66b27a2ca38ab8af6d6c0c5e6b1e9d799c14c1
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-13 17:13:18 -0500

    condition added

---
+++ 07-conditions.sh
@@ -0,0 +1,11 @@
+    #!/bin/bash
+
+    NUMNER=$1
+
+    if[ $NUMNER -gt 100 ]
+
+    then 
+       echo "given number greater than 100"
+    else
+       echo "given number less than 100"
+    fi    
