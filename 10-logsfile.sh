6ab5d684a719046fc08d2b75a9f1e7b6ca471702
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-13 18:34:25 -0500

    condition added

---
+++ 10-logsfile.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+ID=(id -u)
+TIMESTAMP=$(date+%H:%M:%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script is started to executing: $TIMESTAMP"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo "ERROR: $2.. is $R failed $N"
+    exit 1
+    else
+    echo "$2  is $G success $N"
+    fi    
+}
+
+if [ $ID -ne 0 ]
+then 
+   echo "ERROR:please login with root acces"
+   exit 1
+else
+   echo "you are root user"
+fi
+
+yum install mysql -y &>> LOGFILE
+
+VALIDATE $? "install mysql"
+
+yum install git -y &>> LOGFILE
+
+VALIDATE $? "install git"
+
+
+
