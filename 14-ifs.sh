6ff3b02f8e32279909be1d75e315670e01d54fbb
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-18 14:53:41 -0500

    delete old log files

---
+++ 14-ifs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file ="/etc/passwd"
+
+if [ ! -f $file ]
+then
+ echo "file $file does not exist"
+fi
+
+while IFS ":" read -r username password user_id group_id user_fullname home_dir shell_path
+
+do
+echo " username: $username "
+echo "user ID: $user_id"
+echo "User Full name: $user_fullname"
+
+done < $file
