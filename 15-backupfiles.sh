8ff14167ccbd0fcaf6ca0eae5ac678f5dbe242ab
MamathaJaldu <88569465+MamathaJaldu@users.noreply.github.com>
2023-12-18 17:43:59 -0500

    delete old log files

--- 15-backupfiles.sh
+++ 15-backupfiles.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-Backuptime=$(date+%b-%d-%y)
+Backuptime='date+%b-%d-%y'
 
 SOURCE_DIR="/tmp/logfiles"
 
 DESTINATION_DIR="/tmp/logfiles/backup-$Backuptime.tar.gz
 
 # check source dir exist or not
+
 if [ ! -d $SOURCE_DIR ]
 then 
-echo "source directory $SOURCE_DIR does not exist" 
+  echo "source directory $SOURCE_DIR does not exist" 
 fi
 
-tar -cvzf $DESTINATION_DIR $SOURCE_DIR   #create backup
+tar -cvzf $DESTINATION_DIR $SOURCE_DIR 
 
